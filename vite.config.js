6f268fa71d8246769157269903d958340d6d8953
Richard Gitau <richardchegegitau@gmail.com>
2024-10-18 17:32:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://tastebuddy-alx-project.netlify.app/', 
   plugins: [react()],
   optimizeDeps: {
     include: ['@fortawesome/fontawesome-free/css/all.min.css'],
@@ -11,7 +11,7 @@ export default defineConfig({
     rollupOptions: {
       external: [
         '@fortawesome/fontawesome-free',
-        'react-icons/fa', 
+        'react-icons/fa',
       ],
     },
   },
