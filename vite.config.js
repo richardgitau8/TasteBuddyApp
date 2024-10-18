43fd0dff5d2c93d8813b6c8b282bf80bd226eee3
Richard Gitau <richardchegegitau@gmail.com>
2024-10-18 18:16:44 +0300

    removed base url

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://tastebuddy-alx-project.netlify.app/', 
+  base: '', 
   plugins: [react()],
   optimizeDeps: {
     include: ['@fortawesome/fontawesome-free/css/all.min.css'],
